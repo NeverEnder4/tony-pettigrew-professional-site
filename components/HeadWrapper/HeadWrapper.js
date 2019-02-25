0c207c24ae0cc0364bc649d78dcbc487435b2cba
NeverEnder4 <apettigrew.wsdev@gmail.com>
2019-02-25 06:11:54 -0800

    init commit

---
+++ components/HeadWrapper/HeadWrapper.js
@@ -0,0 +1,25 @@
+import Head from 'next/head';
+
+const HeadWrapper = ({ children }) => {
+  return (
+    <div>
+      <Head>
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta httpEquiv="X-UA-Compatible" content="ie=edge" />
+
+        <title>
+          Tony Pettigrew | Web Developer | UX/UI Designer | Creative Solutions
+          Expert
+        </title>
+        <link
+          href="https://fonts.googleapis.com/css?family=Rubik:400,500,700,900"
+          rel="stylesheet"
+        />
+      </Head>
+      {children}
+    </div>
+  );
+};
+
+export default HeadWrapper;
---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import HeadWrapper from '../components/HeadWrapper/HeadWrapper';
+
+import 'normalize.css';
+import '../style.scss';
+
+const index = () => (
+  <HeadWrapper>
+    <h1>I build websites that *pop*</h1>
+    <p>Hello!</p>
+  </HeadWrapper>
+);
+
+export default index;
