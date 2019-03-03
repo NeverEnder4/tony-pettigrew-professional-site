71eb2fea284d19546a14c3d43086b7823c0f3e53
NeverEnder4 <apettigrew.wsdev@gmail.com>
2019-03-03 07:07:24 -0800

    add mobile  menu and screen cover to block content behind menu

--- components/Layout/Layout.js
+++ components/Layout/Layout.js
@@ -3,20 +3,40 @@ import Menu from './Menu/Menu';
 import Footer from './Footer/Footer';
 import ContentWrapper from './ContentWrapper/ContentWrapper';
 
+import './Layout.scss';
+
 class Layout extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      menuOpen: false,
+    };
   }
 
+  menuButtonClickHandler = e => {
+    if (e.target.classList.contains('screen-cover') && this.state.menuOpen) {
+      this.setState(prevState => ({
+        menuOpen: !prevState.menuOpen,
+      }));
+    }
+
+    if (e.target.classList.contains('menu-icon')) {
+      this.setState(prevState => ({
+        menuOpen: !prevState.menuOpen,
+      }));
+    }
+  };
+
   render() {
+    const { menuOpen } = this.state;
     const { children } = this.props;
     return (
-      <React.Fragment>
-        <Header />
+      <div className="layout" onClick={this.menuButtonClickHandler}>
+        <Header menuButtonClickHandler={this.menuButtonClickHandler} />
         <ContentWrapper>{children}</ContentWrapper>
-        <Menu />
+        <Menu isOpen={menuOpen} />
         <Footer />
-      </React.Fragment>
+      </div>
     );
   }
 }
--- components/Layout/Menu/Menu.js
+++ components/Layout/Menu/Menu.js
@@ -1,3 +1,56 @@
-const Menu = () => <nav>MENUUUUU</nav>;
+import Link from 'next/link';
+
+import './Menu.scss';
+
+const Menu = ({ isOpen }) => {
+  const activeClass = isOpen ? 'active' : '';
+  return (
+    <React.Fragment>
+      <div className={`screen-cover ${activeClass}`} />
+      <div className={`menu ${activeClass}`}>
+        <Link href="/">
+          <a>
+            <img
+              src="/static/svg/menu-logo.svg"
+              alt="T P"
+              className="menu-logo"
+            />
+          </a>
+        </Link>
+        <nav className="nav-menu">
+          <ul>
+            <li>
+              <Link href="/biographique">
+                <a className="nav-link">Biographique</a>
+              </Link>
+            </li>
+            <li>
+              <Link href="/works">
+                <a className="nav-link">Works</a>
+              </Link>
+            </li>
+            <li>
+              <Link href="/thoughts">
+                <a href="/thoughts" className="nav-link">
+                  Thoughts
+                </a>
+              </Link>
+            </li>
+            <li>
+              <a href="mailto:apettigrew.wsdev@gmail.com" className="nav-link">
+                Tacoboutit
+              </a>
+            </li>
+          </ul>
+        </nav>
+        <img
+          src="/static/svg/sphere-red-black.svg"
+          alt="red and black sphere"
+          className="sphere-red-black"
+        />
+      </div>
+    </React.Fragment>
+  );
+};
 
 export default Menu;
