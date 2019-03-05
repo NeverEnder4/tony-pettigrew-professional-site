bd3a8546ae5f357db07176dfe45ecfe095aa55b0
NeverEnder4 <apettigrew.wsdev@gmail.com>
2019-03-05 06:14:10 -0800

    Add react-waypoint to reveal works items on scroll, animate works page title to slide in on load

--- components/WorksList/WorksItem/WorksItem.js
+++ components/WorksList/WorksItem/WorksItem.js
@@ -1,39 +1,61 @@
+import { Waypoint } from 'react-waypoint';
+
 import './WorksItem.scss';
 
-const WorksItem = ({ item }) => (
-  <li className="works-gallery-item">
-    <img className="item-image" src={item.imgUrl} alt={item.imgAlt} />
-    <div className="text-content">
-      {item.link ? (
-        <a
-          className="item-link"
-          href={item.href}
-          rel="noreferrer"
-          target="_blank"
-        >
-          <h2 className="item-title">{item.title}</h2>
+class WorksItem extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      itemEntered: false,
+    };
+  }
 
-          <img
-            src="/static/svg/open-new-window.svg"
-            alt="open link in a new window"
-            className="new-window"
-          />
-        </a>
-      ) : (
-        <h2 className="item-title">{item.title}</h2>
-      )}
+  enterHandler = () => {
+    this.setState(() => ({ itemEntered: true }));
+    console.log('Entered!');
+  };
+  render() {
+    const { item } = this.props;
+    const { itemEntered } = this.state;
+    const enteredClass = itemEntered ? 'entered' : '';
+    return (
+      <Waypoint onEnter={this.enterHandler} bottomOffset="55%">
+        <li className={`works-gallery-item ${enteredClass}`}>
+          <img className="item-image" src={item.imgUrl} alt={item.imgAlt} />
+          <div className="text-content">
+            {item.link ? (
+              <a
+                className="item-link"
+                href={item.href}
+                rel="noreferrer"
+                target="_blank"
+              >
+                <h2 className="item-title">{item.title}</h2>
 
-      <div className="item-info">
-        <div className="stats">
-          <h3 className="item-type">{item.style}</h3>
-          <p className="item-tech">{item.tech}</p>
-        </div>
-        <div className="item-description">
-          <p>{item.description}</p>
-        </div>
-      </div>
-    </div>
-  </li>
-);
+                <img
+                  src="/static/svg/open-new-window.svg"
+                  alt="open link in a new window"
+                  className="new-window"
+                />
+              </a>
+            ) : (
+              <h2 className="item-title">{item.title}</h2>
+            )}
+
+            <div className="item-info">
+              <div className="stats">
+                <h3 className="item-type">{item.style}</h3>
+                <p className="item-tech">{item.tech}</p>
+              </div>
+              <div className="item-description">
+                <p>{item.description}</p>
+              </div>
+            </div>
+          </div>
+        </li>
+      </Waypoint>
+    );
+  }
+}
 
 export default WorksItem;
